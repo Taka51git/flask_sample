04cdcaaa2bf39876dc4fcc86de92d9c00b9fdb45
Taka51期 <sh.takanori@gmail.com>
2025-07-15 01:50:20 +0900

    form.html追加

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,17 +3,15 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    books = [
-        {'title': 'はらぺこあおむし',
-         'price': '1200',
-         'arrival_day': '2020年7月12日'},
-        {'title': 'ぐりとぐら',
-         'price': 990,
-         'arrival_day': '2020年7月13日'},
-    ]
-
-
+    books = []
     return render_template(
         'index.html',
         books = books
+    )
+
+
+@app.route('/form')
+def form():
+    return render_template(
+        'form.html'
     )
